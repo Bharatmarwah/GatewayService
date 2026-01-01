b4365fb43f24446428ea48180eb45f0f5cf82e5e
BharatMarwah <bharatmarwah4@gmail.com>
2026-01-02 04:24:49 +0530

    feat: Implemented gateway service including token validation and extraction

---
+++ GatewayService/src/main/java/in/bm/GatewayService/FILTER/GlobalFilter.java
@@ -0,0 +1,59 @@
+package in.bm.GatewayService.FILTER;
+
+import in.bm.GatewayService.SERVICE.JwtFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class GlobalFilter implements org.springframework.cloud.gateway.filter.GlobalFilter, Ordered {
+
+    private JwtFilter jwtFilter;
+
+    public GlobalFilter(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+        if (publicPath(path)) {
+            return chain.filter(exchange);
+        }
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7).trim();
+        if (!jwtFilter.validateToken(token)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String userID = jwtFilter.extractId(token);
+
+        ServerWebExchange mutatedExchange =  exchange.mutate()
+                .request(r->r
+                        .headers(h->h
+                                .add("X_USER_ID",userID))).build();
+
+        return chain.filter(mutatedExchange);
+    }
+    public Boolean publicPath(String path) {
+        return path.startsWith("/") || path.startsWith("/");
+
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
---
+++ GatewayService/src/main/java/in/bm/GatewayService/SERVICE/JwtFilter.java
@@ -0,0 +1,46 @@
+package in.bm.GatewayService.SERVICE;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtFilter {
+
+    private static final String SECRET_KEY = "YjkxYTczZDgzN2I2NDk4N2MxZWMyNGUwODZjYmY4YzIyZjQzMDE5OWE2NmQ2N2E5M2Q0MzFjYTFmYzU0MGI3MQ==";
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return "ACCESS".equals(claims.get("type", String.class));
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String extractId(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+}
