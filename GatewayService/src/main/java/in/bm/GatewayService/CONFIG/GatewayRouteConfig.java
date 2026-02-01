473a98226429731fe7a6dab5dbab11f17e971e3a
BharatMarwah <bharatmarwah4@gmail.com>
2026-02-02 02:49:25 +0530

    feat: add retry filter to gateway routing for resilience

--- GatewayService/src/main/java/in/bm/GatewayService/CONFIG/GatewayRouteConfig.java
+++ GatewayService/src/main/java/in/bm/GatewayService/CONFIG/GatewayRouteConfig.java
@@ -1,6 +1,4 @@
 package in.bm.GatewayService.CONFIG;
-
-
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -14,28 +12,27 @@ public class GatewayRouteConfig {
 
         return builder.routes()
 
-
                 .route("auth",
                         r -> r.path("/auth/public/**")
-                                .filters(f -> f.stripPrefix(1))
+                                .filters(f -> f.stripPrefix(1).retry(2))
                                 .uri("lb://AUTHSERVICE")
                 )
 
 
                 .route("movies",
                         r -> r.path("/movie/movies/**")
-                                .filters(f -> f.stripPrefix(1))
+                                .filters(f -> f.stripPrefix(1).retry(2))
                                 .uri("lb://MOVIESERVICE")
                 )
 
                 .route("theaters",
                         r -> r.path("/theater/theaters/**")
-                                .filters(f -> f.stripPrefix(1))
+                                .filters(f -> f.stripPrefix(1).retry(2))
                                 .uri("lb://MOVIESERVICE")
                 )
                 .route("shows",
                         r->r.path("/show/shows/**")
-                                .filters(f->f.stripPrefix(1))
+                                .filters(f->f.stripPrefix(1).retry(2))
                                 .uri("lb://MOVIESERVICE"))
 
                 .build();
